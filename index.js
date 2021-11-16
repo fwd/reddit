2c9cb5948051aa46ccbddf43fa1a5b2063173d2c
Fresh Web Designs <esteban@fwd.dev>
2021-11-16 14:07:47 -0500

    dataset

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-const fs = require('fs')
 const server = require('@fwd/api')
-
-const csv = require('csvtojson')
-
-function dataset(name) {
-	return new Promise((resolve, reject) => {
-		try {
-			csv().fromFile(`./datasets/${name || 'headlines'}.csv`).then((data)=> resolve(data))
-		} catch(e) { console.log("File not found."); reject() }
-	})
-}
 
 server.add({
 	method: 'get',
 	path: '/json',
-	action: async (req) => await dataset(req.query.name)
+	action: async (req) => require('./headlines.json')
 })
 
 server.start(8080)
